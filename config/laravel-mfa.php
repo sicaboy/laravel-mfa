a43a2bf205c2cc7b16648bd4541c06a7ba998269
sicaboy <sica@qq.com>
2020-04-12 23:54:11 +1000

    Change to namespace way of template view filename

--- config/laravel-mfa.php
+++ config/laravel-mfa.php
@@ -4,7 +4,7 @@ return [
 
     'multi_factor_authentication' => [
         'enabled' => false,
-        'template' => 'laravel-mfa.mfa.form',
+        'template' => 'laravel-mfa::mfa.form',
         'code_expire_after_minutes' => 10,
         'email' => [
             //'mailable' => Sicaboy\LaravelMFA\Mail\AuthenticationCodeMailable::class,
